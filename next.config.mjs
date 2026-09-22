619e3acb3f8b08fa8c19f7c7cd77a7195c16dfd1
AbdurRahman-05 <massabdur123@gmail.com>
2026-09-22 16:17:09 +0530

    cart update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; base-uri 'self'; font-src 'self' https: data:; form-action 'self'; frame-ancestors 'self'; img-src 'self' data:; object-src 'none'; script-src 'self' 'unsafe-inline' https://analytics-dashboard-wtil.onrender.com; script-src-attr 'none'; style-src 'self' https: 'unsafe-inline'; connect-src 'self' https://analytics-dashboard-wtil.onrender.com; upgrade-insecure-requests"
+          }
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
